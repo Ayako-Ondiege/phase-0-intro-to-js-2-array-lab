d5fb088cc7da062878bd4b95f418180f9be3449c
jagedos <jagedosusrecomm@gmail.com>
2024-07-16 14:53:53 +0300

    first attempt

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+
+    return cats.push(name);
+}
+
+destructivelyAppendCat("Ralph");
+
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+destructivelyPrependCat("Bob");
+
+function  destructivelyRemoveLastCat(){
+    return cats.splice(-1);
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift(0,1);
+}
+
+function appendCat(){
+    return [...cats,"Broom"];
+}
+
+function prependCat(){
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat(){
+
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
